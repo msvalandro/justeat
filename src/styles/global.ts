2ad9fa245c786c8c6a8db1e5195b025249a3e7eb
Matheus Valandro <msvalandro@Matheuss-MacBook-Pro.local>
2022-04-12 19:23:43 -0300

    initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --yellow: #ffeaa7;
+
+    --background: #f0f2f5;
+    --shape: #fff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
